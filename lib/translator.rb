658be38f4dc4e6ffa4b3f3287fa01220546655ca
Maylene Poulsen <mayleneski@hotmail.com>
2019-12-25 22:24:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(yaml_file)
   YAML.load_file(yaml_file).each do |meaning, array|
      english = array[0]
      japanese = array[1]
-     emoticons_hash["get_meaning"][ = meaning
+     emoticons_hash["get_meaning"][japanese] = meaning
       emoticans_hash["get_emoticon"][ = array[1]
   end 
   emoticons_hash
