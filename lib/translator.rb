b27d6e71a91edff75347da6a4aa4dfeb2d85ba3e
Maylene Poulsen <mayleneski@hotmail.com>
2019-12-25 22:06:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,16 @@
-# require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(yaml_file)
+  emoticons_hash = {
+    "get_meaning" => {},
+    "get_emoticon" => {}
+  }
+  emoticons = YAML.load_file(yaml_file)
+  emoticons.each do |meaning, array|
+     emoticons_hash["get_meaning"][array[1]] = meaning
+      emoticans_hash["get_emoticon"][array[0]] = array[1]
+  end 
+  emoticons_hash
 end
 
 def get_japanese_emoticon
