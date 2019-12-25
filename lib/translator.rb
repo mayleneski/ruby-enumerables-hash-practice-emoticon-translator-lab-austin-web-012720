2205ac5621c352f6eef6e1b95e164bd5926d6422
Maylene Poulsen <mayleneski@hotmail.com>
2019-12-25 22:42:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'pry'
 
 def load_library(yaml_file)
   emoticons_hash = {
@@ -14,6 +15,7 @@ def load_library(yaml_file)
      emoticons_hash["get_emoticon"][english] = japanese
   end 
   emoticons_hash
+  binding.pry
 end
 
 def get_japanese_emoticon(yaml_file, emoticon)
